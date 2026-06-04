4378ba772f5317b079de741356c196e85670a079
victorGutyy <vicalf0424@gmail.com>
2026-06-04 17:57:48 -0500

    fix: nombre Facebook actualizado en footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -86,7 +86,7 @@ export default function Footer() {
               </li>
               <li>
                 <a
-                  href="https://www.facebook.com/profile.php?id=61590420396146"
+                  href="https://www.facebook.com/yolohagoquindio"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="hover:text-white transition-colors flex items-center gap-2"
@@ -94,7 +94,7 @@ export default function Footer() {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 flex-shrink-0" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M22 12c0-5.522-4.477-10-10-10S2 6.478 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987H7.898V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"/>
                   </svg>
-                  Síguenos en Facebook
+                  Yo Lo Hago Quindío
                 </a>
               </li>
             </ul>
